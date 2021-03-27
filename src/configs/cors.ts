5955598bffdddb175d995f1cc048eabb8bb4c8f5
Daniel Mesquita <danielmesquitta123@gmail.com>
2021-03-27 12:42:05 -0300

    added whitelist to env variables

--- src/configs/cors.ts
+++ src/configs/cors.ts
@@ -1,17 +1,18 @@
 import { CorsOptions } from 'cors';
+import { config as dotenv } from 'dotenv';
 
-const whitelist = ['https://danielmesquitta.vercel.app'];
+dotenv();
+
+const whitelist = JSON.parse(String(process.env.CORS_WHITELIST)) as string[];
+console.log(whitelist);
 
 const corsConfig = {
   origin(origin, callback) {
-    if (
-      whitelist.indexOf(String(origin)) !== -1 ||
-      whitelist.indexOf(`${origin}/`) !== -1
-    ) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
+    console.log(origin);
+    if (whitelist.indexOf(String(origin)) !== -1) {
+      return callback(null, true);
     }
+    return callback(new Error('Not allowed by CORS'));
   },
 } as CorsOptions;
 
